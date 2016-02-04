0b36af7f25a1ffefc5eb3784f219969b36e055bd
takuseno <takuma.seno@gmail.com>
2016-02-04 23:15:24 +0900

    set Kotlin on CalendarView module

---
+++ calendarview/src/main/kotlin/com/shiftize/calendarview/CalendarView.kt
@@ -0,0 +1,9 @@
+package com.shiftize.calendarview
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by takuma on 2016/02/04.
+ */
+class CalendarView(context: Context) : View(context)
